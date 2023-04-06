8dd2b3542b8da98c85cb9ed14bf6d29d9d222165
David Chaban <davidchaban01@gmail.com>
2023-04-05 23:51:44 -0300

    se agrega controller.js

--- servicios/productos-servicios.js
+++ servicios/productos-servicios.js
@@ -1,12 +1,42 @@
 //get
 const listaProductos = () => {
-    fetch(" http://localhost:3000/producto")
+    fetch("http://localhost:3000/producto")
     .then(response => response.json())
     .catch(error => console.log(error))
 }
 
+// post
+
+const crearProductos = (image, name, price) => {
+    fetch('http://localhost:3000/producto',{
+    method:" POST",
+    headers: {
+            "Content-type": "application/son"
+    },
+    body: JSON.stringify({
+        image,
+        price,
+        name
+    })
+    }).then(response => {
+        if(response.ok){
+            return response.body
+        }
+    })
+    throw new Error("No se pudo crear el producto")
+
+}
+
+
+
+
+
+
+
+
 export const productoServicios = {
-    listaProductos
+    listaProductos,
+    crearProductos
 }
 
 
